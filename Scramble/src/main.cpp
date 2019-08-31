393eadb2ff9b98d64b46427e372f701af5b114e3
derrick.timmermans@outlook.com <derrick.timmermans@outlook.com>
2019-08-31 11:44:57 +0800

    Open blank window

--- Scramble/src/main.cpp
+++ Scramble/src/main.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
 #include "Framework.h"
+#include "entt/entt.hpp"
+using namespace bloom;
+
+Game* game = nullptr;
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello world!!" << std::endl;
+	game = new Game({ 800, 600 }, { 0, 0 | SDL_RENDERER_TARGETTEXTURE });
+	try {
+		game->create("Scramble!!", { SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED });
+	}
+	catch (Exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	SDL_DisplayMode dm;
+	SDL_GetCurrentDisplayMode(0, &dm);
+	std::cout << "Screen width is " << dm.w << ", screen height is " << dm.h << "." << std::endl;
+
+	srand(static_cast<uint32_t>(time(nullptr)));
+	SDL_Color randColor = { static_cast<Uint8>(rand() % 255), static_cast<Uint8>(rand() % 255),
+		static_cast<Uint8>(rand() % 255), static_cast<Uint8>(rand() % 255) };
+	game->setColor(randColor);
+	game->clear();
+	game->render();
+
+	system("pause");
 	return 0;
 }
